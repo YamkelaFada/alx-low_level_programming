54782bbc882f319ed3344ec09326b62b04fdcb94
YamkelaFada <nqabisaqwebani2001@gmail.com>
2023-06-19 11:40:13 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unitstd.h>
+/**
+ * main - let us print aline to the standard err
+ * Return: 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
