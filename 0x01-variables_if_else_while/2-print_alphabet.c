84395ab96d0ad9e3e029564664a3a0a7695a4cc0
YamkelaFada <nqabisaqwebani2001@gmail.com>
2023-06-20 15:54:04 -0700

    print in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 /**
- * main - print in lowercase
+ * main - print lower case
  * Return: 0
  */
 int main(void)
 {
-	char x[];
+	char x;
 
-	x = char[26];
-
-	int i;
-
-
-	for (i = 0; i < 26 ; i++)
-	{
-		putchar(tolower(x[i]), "\n");
-	}
+	x = 'a';
+	putchar(x);
+	putchar(\n);
 	return (0);
-
 }
