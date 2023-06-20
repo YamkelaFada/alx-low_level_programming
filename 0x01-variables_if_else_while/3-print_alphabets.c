1e0409f8505207cf84070553f4368474d1050e54
YamkelaFada <nqabisaqwebani2001@gmail.com>
2023-06-20 16:18:44 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Lowercase and Uppercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a';x <= 'z'; x++)
+		putchar(x);
+	for (x = 'A'; x <= 'Z'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
