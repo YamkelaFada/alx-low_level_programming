fafd4006243b2211a7c8ab34a7a31956379ef33e
YamkelaFada <nqabisaqwebani2001@gmail.com>
2023-06-21 11:25:48 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - numbers of base sixteen in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 58; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
