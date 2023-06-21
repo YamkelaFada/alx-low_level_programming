912ec73192107998087e6877754668d78828ccfd
YamkelaFada <nqabisaqwebani2001@gmail.com>
2023-06-21 16:32:35 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adding two values
+ * @x: stores the first value
+ * @y: stores the second value
+ * Return: sum
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
