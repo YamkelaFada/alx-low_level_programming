8063ec9814057136d1f6faf549b9e86467fadf70
YamkelaFada <nqabisaqwebani2001@gmail.com>
2023-06-21 14:06:44 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - check alphabets
+ * @c: check this char
+ * Return 1 if character found, else 0
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'Z' && c <= 'Z'));
+}
