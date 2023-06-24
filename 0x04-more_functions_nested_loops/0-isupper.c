b09b0014ecb908b6c6dd7024f18ec730621ed8fa
YamkelaFada <nqabisaqwebani2001@gmail.com>
2023-06-23 17:27:55 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  _isupper - uppercase
+ *  @c: pass value
+ *  Return: 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1)
+	else
+		return (0);
+}
