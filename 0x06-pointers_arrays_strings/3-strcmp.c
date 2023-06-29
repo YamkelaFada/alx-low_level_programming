39377d9561f5c027e93058bd37a6dc59a94e9dd9
YamkelaFada <nqabisaqwebani2001@gmail.com>
2023-06-29 16:01:17 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: first value
+ * @s2: second value
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x, y;
+
+	x = 0;
+	y = 0;
+
+	while (s1[x] != '\0' & y == 0)
+	{
+		y = s1[x] - s2[x];
+		x++;
+	}
+	return (y);
+}
