257dd9e9199644c6337a75f65d7895dafb9bba2d
YamkelaFada <nqabisaqwebani2001@gmail.com>
2023-07-19 16:10:16 -0700

    task three

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,26 +1,31 @@
-#include <stdio.h>
 #include "3-calc.h"
+#include <stdio.h>
 #include <string.h>
 
 /**
  * get_op_func - Comparing
- * @s: value in s
+ * @s: pass arg
  * Return: function
-**/
+ */
 
 int (*get_op_func(char *s))(int, int)
 {
-	int i;
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
 
-	i = 0;
-	op_t ops[] = {{"+", op_add}, {"-", op_sub}, {"*", op_mul}, {"/", op_div}, {"%", op_mod}, {NULL, NULL}};
-	while (ops[i].op != NULL)
-	{
-		if (!strcmp(ops[i].op, s))
-		{
-			return (ops[i].f);
-		}
-		i++;
-	}
-	return (NULL);
+int i = 0;
+
+while (ops[i].op != NULL)
+{
+if (!strcmp(ops[i].op, s))
+return (ops[i].f);
+i++;
+}
+return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "3-calc.h"
 #include <string.h>
+#include "3-calc.h"
 
 /**
- * main - simple operation
+ * main - function
  * @argc: num args
  * @argv: Value of args
  * Return: int
@@ -12,21 +12,33 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	if ((strcmp(argv[2], "+") && strcmp(argv[2], "-") && strcmp(argv[2], "*") && strcmp(argv[2], "/") && strcmp(argv[2], "%")))
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) && atoi(argv[3]) == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
-	return (0);
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+if (
+(strcmp(argv[2], "+") &&
+strcmp(argv[2], "-") &&
+strcmp(argv[2], "*") &&
+strcmp(argv[2], "/") &&
+strcmp(argv[2], "%"))
+)
+{
+printf("Error\n");
+exit(99);
+}
+
+if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
+&& atoi(argv[3]) == 0)
+{
+printf("Error\n");
+exit(100);
+}
+
+printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+
+return (0);
 }
