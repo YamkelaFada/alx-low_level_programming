e6f9ed2e3f18e1d3066dda8b73a0f901fdf84336
YamkelaFada <nqabisaqwebani2001@gmail.com>
2023-07-19 16:41:36 -0700

    task 0x0D

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef LIKEMACRO_H
+#define LIKEMACRO_H
+
+/**
+ * File: function
+ * @X: pass value
+ * Desc: header
+ */
+
+#define ABS(X) ((X) < 0 ? -(1) * (X) : (X))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef LIKEMACRO_H
+#define LIKEMACRO_H
+
+/**
+ * File: 4-sum
+ * @x: pass value
+ * @y: pass value
+ * Desc: header
+ */
+
+#define SUM(x, y) (x + y)
+
+#endif
