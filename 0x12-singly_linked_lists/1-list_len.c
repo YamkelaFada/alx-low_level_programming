2ad145a3ad217e73ff8717d859988a6e5023ca21
Ziphozihle1990 <ziphozihlembilase@yahoo.com>
2023-07-27 13:45:41 -0700

    singly linked

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - a function that calculate the number of elements
+ * @h: Pass values
+ * Return: the value int
+ */
+
+size_t list_len(const list_t *h)
+{
+	const list_t *x;
+	unsigned int y = 0;
+	
+	x = h;
+	while (x)
+	{
+		y++;
+		x = x->next;
+	}
+	return (y);
+}
